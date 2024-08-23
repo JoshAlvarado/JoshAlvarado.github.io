e4f7146a8161ae0945caec26f8d1e40c470c7248
Joshua Alvarado <josh.alvarado0328@gmail.com>
2024-08-23 15:11:39 -0700

    updating

--- js/text-autotype.js
+++ js/text-autotype.js
@@ -1,50 +1,32 @@
 var i = 1;
-var j= -1;
+var j = -1;
 var list = ["Machine Learning Engineer"];
-var a=list[0];
-var len = list.length;
-var num = 0;
+var nameText = "Joshua Alvarado";  // Your name to be typed out
+var titleText = list[0];  // Your title to be typed out
+var isTypingName = true;  // Flag to check if name is being typed
 
-function txt(){
-  var strs = a.slice(0,i);
-
-  $("#textdisp").html(strs +'|');
-
-  if(i >= 0)
-  {
-    if (i != a.length)
-      {
-        i++;
-      }
-    else
-      {
-        //Don't delete yourself
-        //i = -1;
-      }
+function txt() {
+  var strs;
+  
+  if (isTypingName) {
+    strs = nameText.slice(0, i);
+    $("#nameText").html(strs + '|');
+  } else {
+    strs = titleText.slice(0, i);
+    $("#textdisp").html(strs + '|');
   }
-  else
-  {
-    if(-1*i != a.length)
-      {
-        //Don't delete yourself
-        //i--;
-      }
-    else
-      {
-        //Don't delete yourself
-        //i=1;
-        //var flag = ret_index(a);
-        //a = list[flag];
-      }
+
+  if (i >= 0) {
+    if (isTypingName && i != nameText.length) {
+      i++;
+    } else if (!isTypingName && i != titleText.length) {
+      i++;
+    } else if (isTypingName && i == nameText.length) {
+      isTypingName = false;
+      $("#nameText").html(nameText);  // Remove cursor from name after typing
+      i = 1;  // Reset counter for title
+    }
   }
 }
 
-function ret_index(a){
-  var index = list.indexOf(a);
-  if (index!= (len-1))
-    {return (index+1);}
-  else
-    {return 0;}
-}
-
-setInterval(txt,100);
+setInterval(txt, 100);
